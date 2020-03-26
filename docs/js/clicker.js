473a87800b198acccfe328e1ab955f1ac95c9c2a
AUTISM81 <54669344+AUTISM81@users.noreply.github.com>
2020-03-26 14:17:13 +0100

    buttons

--- docs/js/clicker.js
+++ docs/js/clicker.js
@@ -3,77 +3,102 @@
  * timer är den timer som räknas upp av requestanimframe
  * den är per frame och används av bonus
  * activebonusar är en array för att hålla koll på vilka bonusar vi har
- * 
+ *
  * update körs av requestanimationframe
  * click(värde) körs för att uppdatera score
  */
 const Clicker = function() {
-    const clicker = {};
-    clicker.score = 0;
-    clicker.timer = 0;
-    clicker.activeBonuses = [];
+  const clicker = {};
+  clicker.score = 0;
+  clicker.timer = 0;
+  clicker.activeBonuses = [];
 
-    clicker.update = function () {
-        clicker.timer++;
-    }
+  clicker.update = function() {
+    clicker.timer++;
+  };
 
-    clicker.click = function (val) {
-        clicker.score += val;
-    }
+  clicker.click = function(val) {
+    clicker.score += val;
+  };
 
-    return clicker;
-}
+  return clicker;
+};
 
 /* Bonus objekt, skapas för varje aktiv bonus
  * duration är antalet intervaller som bonusen ska gälla
  * value är hur mycket score ska öka med
  * intervallen är hur många frames ska löpa mellan att score uppdateas
- * 
+ *
  * update kallas i requestanimationframe
  */
-const Bonus = function (duration, value, interval) {
-    const bonus = {};
-    bonus.duration = duration;
-    bonus.value = value;
-    bonus.interval = interval;
+const Bonus = function(duration, value, interval) {
+  const bonus = {};
+  bonus.duration = duration;
+  bonus.value = value;
+  bonus.interval = interval;
 
-    bonus.update = function (timer) {
-        if(timer % this.interval === 0) {
-            this.duration--;
-            clicker.click(this.value);
-        }
+  bonus.update = function(timer) {
+    if (timer % this.interval === 0) {
+      this.duration--;
+      clicker.click(this.value);
     }
-    return bonus;
-}
+  };
+  return bonus;
+};
 
 // Initiera spelets objekt
 let clicker = Clicker();
 let score;
 
-// Vänta på att sidan ska laddas 
-window.addEventListener('load', (event) => {
-    console.log('page is fully loaded');
+// Vänta på att sidan ska laddas
+window.addEventListener(
+  "load",
+  event => {
+    console.log("page is fully loaded");
 
     // Hämta html element
     let clickerButton = document.querySelector("#clicker");
     let bonusButton = document.querySelector("#bonus");
+    let clickerButton2 = document.querySelector("#clicker2");
+    let bonusButton2 = document.querySelector("#bonus2");
     score = document.querySelector("#score"); // score element
 
     // eventlisteners för knappar med tillhörande funktioner
-    clickerButton.addEventListener('click', (e) => {
+    clickerButton.addEventListener(
+      "click",
+      e => {
         // vid click öka score med 1
         clicker.click(1);
         console.log(clicker.score);
-    }, false);
+      },
+      false
+    );
 
-    bonusButton.addEventListener('click', (e) => {
+    clickerButton2.addEventListener("click", e => {
+      clicker.click(2);
+      console.log(clicker.score);
+    });
+
+    bonusButton.addEventListener(
+      "click",
+      e => {
         // vid click skapa och lägg till denna bonus
         clicker.activeBonuses.push(Bonus(10, 2, 60));
-    },false);
+      },
+      false
+    );
+
+    bonusButton2.addEventListener("click", e => {
+      if (Clicker >= 100) {
+        clicker.activeBonuses.push(Bonus(10, 10, 60));
+        clicker.click(-100);
+      }
+    });
 
     window.requestAnimationFrame(runClicker);
-
-}, false);
+  },
+  false
+);
 
 /*
  * runClicker är vår requestAnimationFrame metod
@@ -82,20 +107,23 @@ window.addEventListener('load', (event) => {
  * webbläsaren köra 60 ggr i sekunden
  */
 function runClicker() {
-    clicker.update(); // uppdatera spelet
+  clicker.update(); // uppdatera spelet
 
-    // gå igenom spelets bonusar och aktivera dem
-    for (let bonus of clicker.activeBonuses) {
-        bonus.update(clicker.timer);
+  // gå igenom spelets bonusar och aktivera dem
+  for (let bonus of clicker.activeBonuses) {
+    bonus.update(clicker.timer);
 
-        // om en bonus löpt ut, ta bort den från arrayen
-        if (bonus.duration <= 0) {
-            clicker.activeBonuses.splice(clicker.activeBonuses.indexOf(x => x === bonus), 1);
-        }
+    // om en bonus löpt ut, ta bort den från arrayen
+    if (bonus.duration <= 0) {
+      clicker.activeBonuses.splice(
+        clicker.activeBonuses.indexOf(x => x === bonus),
+        1
+      );
     }
+  }
 
-    // uppdaterar score texten
-    score.textContent = "Score: " + clicker.score;
+  // uppdaterar score texten
+  score.textContent = "Score: " + clicker.score;
 
-    window.requestAnimationFrame(runClicker);
+  window.requestAnimationFrame(runClicker);
 }
